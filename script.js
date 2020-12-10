24d55ff54c4e0d84f0ccaf15409ffbcb02f0b21c
Rob Donnelly <rfdonnelly@gmail.com>
2020-12-10 11:36:11 -0800

    feat: add audio cue when stetch time is reached

--- script.js
+++ script.js
@@ -47,4 +47,39 @@ function tick() {
     var ms = ticks % 100;
 
     timer_div.innerHTML = s + ":" + ('0' + ms).slice(-2);
+    if (s == 30 && ms == 0) {
+        beep(50, 500, 0.2);
+    }
 }
+
+
+var audio_context = new (window.AudioContext || window.webkitAudioContext || window.audioContext);
+
+// From: https://stackoverflow.com/a/29641185
+// All arguments are optional:
+// * duration of the tone in milliseconds. Default is 500
+// * frequency of the tone in hertz. default is 440
+// * volume of the tone. Default is 1, off is 0.
+// * type of tone. Possible values are sine, square, sawtooth, triangle, and custom. Default is sine.
+// * callback to use on end of tone
+function beep(duration, frequency, volume, type, callback) {
+    var oscillator = audio_context.createOscillator();
+    var gainNode = audio_context.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audio_context.destination);
+
+    if (volume){gainNode.gain.value = volume;}
+    if (frequency){oscillator.frequency.value = frequency;}
+    if (type){oscillator.type = type;}
+    if (callback){oscillator.onended = callback;}
+
+    oscillator.start(audio_context.currentTime);
+    oscillator.stop(audio_context.currentTime + ((duration || 500) / 1000));
+};
+
+document.getElementsByTagName("button")[0].addEventListener("click", function () {
+    var button = this;
+    button.disabled = true;
+
+});
